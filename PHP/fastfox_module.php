0ec16143af8f443b628c491e5280d985e8c478bf
Foxdim <103509067+Foxdim@users.noreply.github.com>
2023-02-10 06:05:48 +0300

    Update fastfox_module.php

--- PHP/fastfox_module.php
+++ PHP/fastfox_module.php
@@ -16,18 +16,20 @@ class FastFox_Foxdim_Module {
     function test_connection()
     {
         $KEY_ARR=$this->KEY_ARR;
-        $PAYLOAD_ARR=["FJWT"=>["REQUEST"=>"test_connection"]];
+        $PAYLOAD_ARR=["FJWT"=>["PAYLOAD"=>["REQUEST"=>"test_connection"]]];
         $FJWT=$this->FJWT_encode($PAYLOAD_ARR);
         $response_FJWT=$this->Api_Request($FJWT);
         $FJWT_decode=$this->FJWT_decode($response_FJWT);
         return $FJWT_decode;
+        
+        
     }
     
     
     function show_packages($barcodes) //barcode1,barcode2,barcode3 vs vs.
     {
         $KEY_ARR=$this->KEY_ARR;
-        $PAYLOAD_ARR=["FJWT"=>["REQUEST"=>"show_packages","BARCODES"=>$barcodes]];
+        $PAYLOAD_ARR=["FJWT"=>["PAYLOAD"=>["REQUEST"=>"show_packages","BARCODES"=>$barcodes]]];
         $FJWT=$this->FJWT_encode($PAYLOAD_ARR);
         $response_FJWT=$this->Api_Request($FJWT);
         $FJWT_decode=$this->FJWT_decode($response_FJWT);
@@ -37,7 +39,7 @@ class FastFox_Foxdim_Module {
     function show_last_packages($limit=5) //max 1000 packages
     {
         $KEY_ARR=$this->KEY_ARR;
-        $PAYLOAD_ARR=["FJWT"=>["REQUEST"=>"show_last_packages","LIMIT"=>$limit]];
+        $PAYLOAD_ARR=["FJWT"=>["PAYLOAD"=>["REQUEST"=>"show_last_packages","LIMIT"=>$limit]]];
         $FJWT=$this->FJWT_encode($PAYLOAD_ARR);
         $response_FJWT=$this->Api_Request($FJWT);
         $FJWT_decode=$this->FJWT_decode($response_FJWT);
@@ -47,7 +49,7 @@ class FastFox_Foxdim_Module {
     {
         
         $KEY_ARR=$this->KEY_ARR;
-        $PAYLOAD_ARR=["FJWT"=>["REQUEST"=>"create_package","PACKAGE"=>$package_arr]];
+        $PAYLOAD_ARR=["FJWT"=>["PAYLOAD"=>["REQUEST"=>"create_package","PACKAGE"=>$package_arr]]];
         $FJWT=$this->FJWT_encode($PAYLOAD_ARR);
         $response_FJWT=$this->Api_Request($FJWT);
         $FJWT_decode=$this->FJWT_decode($response_FJWT);
@@ -57,7 +59,7 @@ class FastFox_Foxdim_Module {
     function delete_packages($barcodes)
     {
         $KEY_ARR=$this->KEY_ARR;
-        $PAYLOAD_ARR=["FJWT"=>["REQUEST"=>"delete_packages","BARCODES"=>$barcodes]];
+        $PAYLOAD_ARR=["FJWT"=>["PAYLOAD"=>["REQUEST"=>"delete_packages","BARCODES"=>$barcodes]]];
         $FJWT=$this->FJWT_encode($PAYLOAD_ARR);
         $response_FJWT=$this->Api_Request($FJWT);
         $FJWT_decode=$this->FJWT_decode($response_FJWT);
@@ -125,11 +127,14 @@ class FastFox_Foxdim_Module {
         return json_encode($text,JSON_UNESCAPED_UNICODE);
     }
     
-    function FJWT_encode($PAYLOAD_ARR)
+   function FJWT_encode($PAYLOAD_ARR)
     {
         $KEY_ARR=$this->KEY_ARR;
-        $PAYLOAD_ARR["FJWT"]=$this->encode($this->get_utc()).".".$KEY_ARR["PUBLIC_KEY"].".".$this->encode($this->jsonEncode($PAYLOAD_ARR["FJWT"]));
-        $PAYLOAD_ARR_JSON=$this->jsonEncode($PAYLOAD_ARR);
+        $RETURN_ARR=$PAYLOAD_ARR;
+        $RETURN_ARR["FJWT"]["TOKEN"]=$this->encode($this->get_utc());
+        $RETURN_ARR["FJWT"]["KEY"]=$KEY_ARR["PUBLIC_KEY"];
+        $RETURN_ARR["FJWT"]["PAYLOAD"]=$this->encode($this->jsonEncode($PAYLOAD_ARR["FJWT"]["PAYLOAD"]));
+        $PAYLOAD_ARR_JSON=$this->jsonEncode($RETURN_ARR);
         return $PAYLOAD_ARR_JSON;
     }
     function FJWT_decode($FJWT_JSON)
@@ -137,11 +142,11 @@ class FastFox_Foxdim_Module {
         $KEY_ARR=$this->KEY_ARR;
         $FJWT=$FJWT_JSON;
         if(!is_array($FJWT))$FJWT=$this->jsonDecode($FJWT_JSON);
-        $FJWT_ARR=explode(".",$FJWT["FJWT"]);
-        $PAYLOAD=$this->jsonDecode($this->decode($FJWT_ARR[2]));
-        $FJWT["FJWT"]=$PAYLOAD;
+        $PAYLOAD=$this->jsonDecode($this->decode($FJWT["FJWT"]["PAYLOAD"]));
+        $FJWT["FJWT"]["PAYLOAD"]=$PAYLOAD;
         return $FJWT;
     }
+    
     private $ciphering = "AES-128-CBC";
     function encode($text)
     {
@@ -186,6 +191,5 @@ class FastFox_Foxdim_Module {
         $newstring = mb_convert_case($newstring, MB_CASE_LOWER, "UTF-8");
         return $newstring;
     }
-    
 }
 ?>
